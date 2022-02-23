801fba7d00c33c560b66621f87b48a87f9af1a6e
Missquinn <quintabellanjeh@yahoo.com>
2022-02-22 19:09:01 -0500

    my second commit

---
+++ builtins.py
@@ -0,0 +1,15 @@
+property 
+the property() takes four parameters:
+fget - function for geting an attribute value
+fset - function for setting an attribute value
+fdel - function for deleting an attribute value
+doc - a string that contains documents
+
+range
+the range() function returns a sequence of numbers, starting from 0 by default
+and cremetal by 1 by default  and stops before a  specified number.
+
+reversed
+the reversed() function returns an iterator that accesses the given sequence in
+in the reverse order.
+  
